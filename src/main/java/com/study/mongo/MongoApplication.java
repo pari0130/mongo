0b894df365dfc0b4ddac4fafe14f0259e6cefb27
donghwi <pari0130@gmail.com>
2021-04-30 21:35:52 +0900

    [설정] 몽고디비 사용 테스트

--- src/main/java/com/study/mongo/MongoApplication.java
+++ src/main/java/com/study/mongo/MongoApplication.java
@@ -1,13 +1,27 @@
 package com.study.mongo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableJpaAuditing
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+@RequiredArgsConstructor
 @SpringBootApplication
 public class MongoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MongoApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
